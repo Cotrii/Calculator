0bd4987a0915f6a074074a0159b539fec3fa298e
Cotrii <carlomiguelcortez@gmail.com>
2022-07-30 21:57:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(a, b){
-    return a + b;
+    return Math.round(a + b);
 }
 
 function subtract(a, b){
-    return a - b;
+    return Math.round(a - b);
 }
 
 function multiply(a, b){
-    return a * b;
+    return Math.round(a * b);
 }
 
 function divide(a, b){
-    return a / b;
+    return Math.round(a / b);
 }
 
 
