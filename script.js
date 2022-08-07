420171ed8bb14a5d97d754bf74966810e24bb74d
Cotrii <carlomiguelcortez@gmail.com>
2022-08-07 19:25:37 +0800

    added evaluate func

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ function divide(a, b){
 
 
 function operate(total, nextNum, optStr){
+
+    total = Number(total);
+    nextNum = Number(nextNum);
     
     if (optStr === '+')      total = add(total, nextNum);
     else if (optStr === '-') total = subtract(total, nextNum);
@@ -25,25 +28,32 @@ function operate(total, nextNum, optStr){
         // else 
         total = divide(total, nextNum);
     };
+
+    return total.toString();
 }
 
-
-console.log(document.querySelector('[data-num]').textContent);
 
 const numButtons = document.querySelectorAll('[data-num]'); //number buttons
 const currNum = document.querySelector(".currNum"); //current Num
 
+const prevNum = document.querySelector(".prevNum");
+
+
 const optButtons = document.querySelectorAll(".opt");
-let currentOpt;
-let prevOpt;
+
+const equalsBtn = document.querySelector(".eq");
+// let currentOpt;
+let currOpt = document.querySelector(".currOpt");
 
 numButtons.forEach((btn) => {
     btn.addEventListener("click", changeCurrentNum);
 })
 
 optButtons.forEach((btn) => {
-    // btn.addEventListener("click", changeCurrentNum);
+    btn.addEventListener("click", setOpt);
 })
+
+equalsBtn.addEventListener("click", evaluate);
 
 function changeCurrentNum(){
 
@@ -52,6 +62,37 @@ function changeCurrentNum(){
     } else {
         currNum.textContent += this.textContent;
     }
+}
+
+let num = 0;
+
+function setOpt(){
+
+    if (currOpt.textContent === ''){
+        currOpt.textContent = this.textContent;
+
+        if (prevNum.textContent === '' ){
+            prevNum.textContent = currNum.textContent;
+            currNum.textContent = '0';
+        } 
+    } 
+    else {
+            num = operate(prevNum.textContent, currNum.textContent, currOpt.textContent);
+
+            prevNum.textContent = num;
+            currOpt.textContent = this.textContent;
+            //this is setOp, so assume that there is a next operator in line
+
+            currNum.textContent = '0';
+    }
+}
+
+function evaluate(){
+    num = operate(prevNum.textContent, currNum.textContent, currOpt.textContent);
+
+    currNum.textContent = num;
+    prevNum.textContent = null;
+    currOpt.textContent = null;
 }
 
 
