4f6f7ed02c2a6f176effe487ed5bec82a912649a
Cotrii <carlomiguelcortez@gmail.com>
2022-08-07 18:04:10 +0800

    class name changes

--- script.js
+++ script.js
@@ -20,17 +20,29 @@ function operate(total, nextNum, optStr){
     if (optStr === '+')      total = add(total, nextNum);
     else if (optStr === '-') total = subtract(total, nextNum);
     else if (optStr === '*') total = multiply(total, nextNum);
-    else if (optStr === '/') total = divide(total, nextNum);
+    else if (optStr === '/') {
+        // if (nextNum == 0) return null;
+        // else 
+        total = divide(total, nextNum);
+    };
 }
 
 
 console.log(document.querySelector('[data-num]').textContent);
 
-const numButtons = document.querySelectorAll('[data-num'); //number buttons
+const numButtons = document.querySelectorAll('[data-num]'); //number buttons
 const currNum = document.querySelector(".currNum"); //current Num
+
+const optButtons = document.querySelectorAll(".opt");
+let currentOpt;
+let prevOpt;
 
 numButtons.forEach((btn) => {
     btn.addEventListener("click", changeCurrentNum);
+})
+
+optButtons.forEach((btn) => {
+    // btn.addEventListener("click", changeCurrentNum);
 })
 
 function changeCurrentNum(){
